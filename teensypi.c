73a0485fda8b4592da63db8e403d817f39a32e1f
mvmende <83758566+mvmende@users.noreply.github.com>
2021-06-03 10:05:22 +0200

    Update and rename teensypi to teensypi.c

--- teensypi.c
+++ teensypi.c
@@ -8,7 +8,7 @@ int main ()
 {
   int fd ;
 
-  if ((fd = serialOpen ("/dev/ttyAMA0", 115200)) < 0)
+  if ((fd = serialOpen ("/dev/ttyUSB0", 9600)) < 0)
   {
     fprintf (stderr, "Unable to open serial device: %s\n", strerror (errno)) ;
     return 1 ;
