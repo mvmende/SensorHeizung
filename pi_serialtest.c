b19f043f50338d88267a86a2aec4d2c62d191ca1
mvmende <83758566+mvmende@users.noreply.github.com>
2021-06-05 10:15:45 +0200

    Update pi_serialtest.c

--- pi_serialtest.c
+++ pi_serialtest.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#inculde <math.h>
 #include <errno.h>
 #include <wiringPi.h>
 #include <wiringSerial.h>
 
 int main ()
-{ float t;
-  char input[5];
+{ int t;
+  char input[10];
   int fd ;
   
  
@@ -30,8 +31,8 @@ int main ()
       for (int i = 0; i<2; i++){
         input[i] = serialGetchar(fd);
       }
-      t = input[0]*10+input[1];
-      printf(input);
+      t = atoi (input);
+      printf("%d",t);
     }
     //putchar (serialGetchar (fd)) ;
     fflush (stdout) ;
