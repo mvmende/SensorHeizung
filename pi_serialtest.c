a0791103e33c2984efa1c2217bfd44d21fb911cc
mvmende <83758566+mvmende@users.noreply.github.com>
2021-06-05 14:12:49 +0200

    Update pi_serialtest.c

--- pi_serialtest.c
+++ pi_serialtest.c
@@ -35,12 +35,12 @@ int main ()
       }
       t = atof (input);
       printf("Temperatur:%.2f \n", t);
-      fflush (stdout) ;
+     // fflush (stdout) ;
     }
      else{
        if (serialGetchar(fd) == 'h'){
         for (int j = 0; j<4; j++){
-          input[j] = serialGetchar(fd);
+          inputh[j] = serialGetchar(fd);
         }
         h = atof (input);
         printf("Luftfeuchtigkeit:%.2f \n", h);
