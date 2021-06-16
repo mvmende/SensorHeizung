d0c4e04a4972363ae49bb6b174c4700fc631a6dd
mvmende <83758566+mvmende@users.noreply.github.com>
2021-06-16 09:38:39 +0200

    Update pi_serialtest2.c

--- pi_serialtest2.c
+++ pi_serialtest2.c
@@ -17,7 +17,7 @@ int main ()
   char in_heiz[10];
   char in_luft[10];
   int fd ;
-  char buffer[50];
+  char buff[50];
   
  
   if ((fd = serialOpen ("/dev/ttyACM0", 9600)) < 0)
@@ -33,10 +33,56 @@ int main ()
       int i = 0;
       while (serialGetchar(fd) != '\r')
       {
-        buffer[i] = serialGetchar(fd);
+        buff[i] = serialGetchar(fd);
         i = i+1;
       }
     }
-    printf(buffer);
+    int j = 0;
+    if (buff[j] == 't')
+    {
+     for (int i = 0; i<4; i++)
+     {
+       in_temp[i] = buff[j];
+       j = j+1;
+     }
+    }
+    
+    if (buff[j] == 'h')
+    {
+     for (int i = 0; i<4; i++)
+     {
+       in_hum[i] = buff[j];
+       j = j+1;
+     }
+    }
+    
+    if (buff[j] == 'a')
+    {
+     for (int i = 0; i<3; i++)
+     {
+       in_heiz[i] = buff[j];
+       j = j+1;
+     }
+    }
+    
+    if (buff[j] == 'b')
+    {
+     for (int i = 0; i<3; i++)
+     {
+       in_luft[i] = buff[j];
+       j = j+1;
+     }
+    }
+    
+    t = atof (in_temp);
+    h = atof (in_hum);
+    heiz = atof (in_heiz);
+    luft = atof (in_luft);
+    printf("Temperatur:%.2f Grad Celsius \n", t);
+    printf("Luftfeuchtigkeit:%.2f %% \n", h);
+    printf("Heizleistung: %.2f %% \n", heiz);
+    printf("Lüfterleistung: %.2f %% \n", luft);
+      
+    
   }
 }
